fac417db18741985932dca7d14c8241d92d62efc
nehaevbogdan16-svg <nehaevbogdan16@gmail.com>
2025-12-14 10:25:40 +0300

    Refactor video URL embedding logic
    Updated the toEmbed function to use template literals for URL construction and improved variable naming for clarity.

--- public/script.js
+++ public/script.js
@@ -4,24 +4,25 @@ const input = document.getElementById("videoUrl");
 const button = document.getElementById("loadVideo");
 const iframe = document.getElementById("videoFrame");
 
-// преобразование любой ссылки в embed
+// преобразуем обычные ссылки в embed
 function toEmbed(url) {
+
   // YouTube
   if (url.includes("youtube.com/watch")) {
     const id = new URL(url).searchParams.get("v");
-    if (id) return `https://www.youtube.com/embed/${id}`;
+    if (id) return "https://www.youtube.com/embed/" + id;
   }
 
   if (url.includes("youtu.be/")) {
     const id = url.split("youtu.be/")[1];
-    return `https://www.youtube.com/embed/${id}`;
+    return "https://www.youtube.com/embed/" + id;
   }
 
   // VK
   if (url.includes("vk.com/video")) {
-    const m = url.match(/video(-?\d+)_(\d+)/);
-    if (m) {
-      return `https://vk.com/video_ext.php?oid=${m[1]}&id=${m[2]}&hd=2`;
+    const match = url.match(/video(-?\d+)_(\d+)/);
+    if (match) {
+      return `https://vk.com/video_ext.php?oid=${match[1]}&id=${match[2]}&hd=2`;
     }
   }
 
@@ -29,13 +30,14 @@ function toEmbed(url) {
   if (url.includes("rutube.ru/video")) {
     const id = url.split("/video/")[1]?.split("/")[0];
     if (id) {
-      return `https://rutube.ru/play/embed/${id}`;
+      return "https://rutube.ru/play/embed/" + id;
     }
   }
 
   return null;
 }
 
+// загрузка видео
 button.onclick = () => {
   const url = input.value.trim();
   const embed = toEmbed(url);
@@ -48,7 +50,7 @@ button.onclick = () => {
   socket.emit("video", embed);
 };
 
+// получение видео всеми участниками
 socket.on("video", (url) => {
   iframe.src = url;
 });
-
