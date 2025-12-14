63e33c9f376aa14e1da20194ecf3f33e40813201
nehaevbogdan16-svg <nehaevbogdan16@gmail.com>
2025-12-14 11:47:36 +0300

    Refactor room management and update server start message

--- server.js
+++ server.js
@@ -6,38 +6,35 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const rooms = {};
 app.use(express.static("public"));
 
+const rooms = {};
+
 io.on("connection", socket => {
-  socket.on("join-room", room => {
+
+  socket.on("join", room => {
     socket.join(room);
+    socket.room = room;
+
     if (!rooms[room]) {
-      rooms[room] = { host: socket.id };
-      socket.emit("role", "host");
-    } else socket.emit("role", "viewer");
+      rooms[room] = {
+        video: null
+      };
+    }
+
+    if (rooms[room].video) {
+      socket.emit("video", rooms[room].video);
+    }
   });
 
-  socket.on("video-load", d => {
-    if (rooms[d.room]?.host === socket.id)
-      socket.to(d.room).emit("video-load", d);
+  socket.on("video", url => {
+    if (!socket.room) return;
+    rooms[socket.room].video = url;
+    socket.to(socket.room).emit("video", url);
   });
 
-  socket.on("video-event", d => {
-    if (rooms[d.room]?.host === socket.id)
-      socket.to(d.room).emit("video-event", d);
-  });
-
-  socket.on("chat", d => socket.to(d.room).emit("chat", d));
-
-  socket.on("offer", d => socket.to(d.room).emit("offer", d));
-  socket.on("answer", d => socket.to(d.room).emit("answer", d));
-  socket.on("ice", d => socket.to(d.room).emit("ice", d));
-
-  socket.on("disconnect", () => {
-    for (const r in rooms)
-      if (rooms[r].host === socket.id) delete rooms[r];
-  });
 });
 
-server.listen(3000, () => console.log("http://localhost:3000"));
+server.listen(process.env.PORT || 3000, () => {
+  console.log("КИНО69 запущен");
+});
