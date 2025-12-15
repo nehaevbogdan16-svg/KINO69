90dfede74940c87c00d203eb84b6d9e02b6c07cc
nehaevbogdan16-svg <nehaevbogdan16@gmail.com>
2025-12-15 06:27:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const path = require("path");
+
+app.get("/room/:room", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "room.html"));
+});
 
 app.use(express.static("public"));
 
