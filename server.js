4a306fa64b101aab9c9e2add479c839408ddd3f6
nehaevbogdan16-svg <nehaevbogdan16@gmail.com>
2025-12-15 03:03:35 +0300

    Remove unused socket event listeners

--- server.js
+++ server.js
@@ -13,44 +13,21 @@ io.on("connection", socket => {
     console.log("Вход в комнату:", room);
   });
 
-  /* ========= ОБЫЧНОЕ IFRAME ВИДЕО ========= */
   socket.on("set-video", data => {
     io.to(data.room).emit("set-video", data);
   });
 
-  /* ========= ЧАТ ========= */
   socket.on("chat", data => {
     io.to(data.room).emit("chat", data);
-  });
-
-  /* ========= YOUTUBE SYNC ========= */
-  socket.on("load-yt", data => {
-    io.to(data.room).emit("load-yt", data);
-  });
-
-  socket.on("yt-play", data => {
-    socket.to(data.room).emit("yt-play", data);
-  });
-
-  socket.on("yt-pause", data => {
-    socket.to(data.room).emit("yt-pause", data);
-  });
-
-  /* ========= WEBRTC SCREEN SHARE ========= */
-  ["offer", "answer", "ice"].forEach(event => {
-    socket.on(event, data => {
-      socket.to(data.room).emit(event, data);
-    });
-  });
-
-  /* ========= СТАРОЕ / МОЖНО УБРАТЬ ========= */
-  socket.on("sync", data => {
-    socket.to(data.room).emit("sync", data);
   });
 
   socket.on("disconnect", () => {
     console.log("Пользователь вышел");
   });
+  socket.on("sync", data => {
+    socket.to(data.room).emit("sync", data);
+  });
+
 });
 
 const PORT = process.env.PORT || 3000;
