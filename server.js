80191c1d4fee64a5d6c8841d2299759b3fc84e1a
nehaevbogdan16-svg <nehaevbogdan16@gmail.com>
2025-12-15 04:04:52 +0300

    Clean up socket event listeners in server.js
    Removed unused event listeners for YouTube sync and video controls.

--- server.js
+++ server.js
@@ -13,40 +13,21 @@ io.on("connection", socket => {
     console.log("Вход в комнату:", room);
   });
 
-  /* ===== ЗАГРУЗКА ВИДЕО (iframe) ===== */
   socket.on("set-video", data => {
     io.to(data.room).emit("set-video", data);
   });
 
-  /* ===== ЧАТ ===== */
   socket.on("chat", data => {
     io.to(data.room).emit("chat", data);
-  });
-
-  /* ===== YOUTUBE IFRAME API SYNC ===== */
-  socket.on("load-yt", data => {
-    // загрузить видео всем
-    io.to(data.room).emit("load-yt", data);
-  });
-
-  socket.on("yt-play", data => {
-    // play у всех, кроме отправителя
-    socket.to(data.room).emit("yt-play", data);
-  });
-
-  socket.on("yt-pause", data => {
-    // pause у всех, кроме отправителя
-    socket.to(data.room).emit("yt-pause", data);
-  });
-
-  /* ===== ОСТАВИЛ ТВОЙ SYNC (если понадобится) ===== */
-  socket.on("sync", data => {
-    socket.to(data.room).emit("sync", data);
   });
 
   socket.on("disconnect", () => {
     console.log("Пользователь вышел");
   });
+  socket.on("sync", data => {
+    socket.to(data.room).emit("sync", data);
+  });
+
 });
 
 const PORT = process.env.PORT || 3000;
