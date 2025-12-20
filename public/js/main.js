1fcf4dd91b55744579cac28d09be6cd12a8bd15f
nehaevbogdan16-svg <nehaevbogdan16@gmail.com>
2025-12-20 23:37:45 +0300

    Delete public directory

--- public/js/main.js
+++
@@ -1,4 +0,0 @@
-let all=[];const c=document.getElementById("movies");
-fetch("/movies").then(r=>r.json()).then(d=>{all=d;render(d)});
-function render(l){c.innerHTML="";l.forEach((m,i)=>c.innerHTML+=`<div class=movie><img src=${m.poster}><h3>${m.title}</h3><a href=movie.html?id=${i}>Смотреть</a></div>`);}
-search.oninput=()=>{const q=search.value.toLowerCase();render(all.filter(m=>m.title.toLowerCase().includes(q)||(m.genre||"").toLowerCase().includes(q)))};
